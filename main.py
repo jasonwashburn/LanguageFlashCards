1417e6385b711d9d47e1bb22c6242c52d1a99bc3
Jason Washburn <wburn@wburn.net>
2021-03-14 06:27:23 -0500

    added card flip logic with timer

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 SMALL_FONT = ("Arial", 40, "italic")
 LARGE_FONT = ("Arial", 60, "bold")
-
+new_word = {}
 # ---------------------------- READ DATA ------------------------------- #
 data_df = pd.read_csv("data/french_words.csv")
 data_dict = data_df.to_dict(orient="records")
 
+
 # ---------------------------- FLASHCARD LOGIC ------------------------------- #
 def new_card():
+    global new_word
     new_word = random.choice(data_dict)
-    canvas.itemconfig(title, text='French')
-    canvas.itemconfig(word, text=new_word.get('French'))
+    canvas.itemconfig(title, fill='black', text='French')
+    canvas.itemconfig(word, fill='black', text=new_word.get('French'))
+    window.after(3000, flip_card)
+
+
+def flip_card():
+    global new_word
+    canvas.itemconfig(card_img, image=card_back_img)
+    canvas.itemconfig(title, fill='white', text='English')
+    canvas.itemconfig(word, fill='white', text=new_word.get('English'))
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,17 +34,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.gif")
 card_back_img = PhotoImage(file="images/card_back.gif")
-canvas.create_image(400, 263, image=card_front_img)
+card_img = canvas.create_image(400, 263, image=card_front_img)
 title = canvas.create_text(400, 150, font=SMALL_FONT, text='French')
 word = canvas.create_text(400, 263, font=LARGE_FONT, text='Word')
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_button_img = PhotoImage(file="images/wrong.gif")
-wrong_button = Button(image=wrong_button_img, command=new_card, borderwidth=0, highlightbackground=BACKGROUND_COLOR, highlightthickness=0)
+wrong_button = Button(image=wrong_button_img, command=new_card, borderwidth=0, highlightbackground=BACKGROUND_COLOR,
+                      highlightthickness=0)
 wrong_button.grid(column=0, row=1)
 
 right_button_img = PhotoImage(file="images/right.gif")
 right_button = Button(image=right_button_img, borderwidth=0, highlightbackground=BACKGROUND_COLOR, highlightthickness=0)
 right_button.grid(column=1, row=1)
 
+new_card()
 window.mainloop()
